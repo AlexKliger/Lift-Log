cbd7ccc3f67ba8105e0189365adae4417fdae3d0
Alex <alex.kliger@gmail.com>
2022-07-22 12:07:08 -0400

    implemented stepper

--- client/src/components/AddSet.js
+++ client/src/components/AddSet.js
@@ -1,26 +1,46 @@
 import {useState} from 'react'
 
-const AddSet = ({id, handleSubmit}) => {
-    const [weight, setWeight] = useState(0)
-    const [reps, setReps] = useState(0)
+const AddSet = ({handleSubmit}) => {
+  const [weight, setWeight] = useState(0)
+  const [reps, setReps] = useState(0)
 
-    function handleChange(e) {
-        if (e.target.id === 'weight') setWeight(e.target.value)
-        if(e.target.id === 'reps') setReps(e.target.value)
-    }
+  function handleChange(e) {
+    if (e.target.parentNode.classList.contains('stepper--weight')) setWeight(e.target.value)
+    if(e.target.parentNode.classList.contains('stepper--reps')) setReps(e.target.value)
+  }
 
-    function onSubmit(e) {
-        handleSubmit(weight, reps)
-        e.preventDefault()
-    }
+  function stepUp(e) {
+    if (e.target.parentNode.classList.contains('stepper--weight')) setWeight(weight + 1)
+    if(e.target.parentNode.classList.contains('stepper--reps')) setReps(reps + 1)
+  }
+  
+  function stepDown(e) {
+    if (e.target.parentNode.classList.contains('stepper--weight')) setWeight(weight - 1)
+    if(e.target.parentNode.classList.contains('stepper--reps')) setReps(reps - 1)
+  }
 
-    return (
-        <form onSubmit={onSubmit}>
-            <input type="number" id='weight' value={weight} placeholder="weight" onChange={handleChange}></input>
-            <input type="number" id='reps' value={reps} placeholder="reps" onChange={handleChange}></input>
-            <input type="submit" value="+"></input>
-        </form>
-    )
+  function onSubmit(e) {
+    handleSubmit(weight, reps)
+    e.preventDefault()
+  }
+
+  return (
+      <form onSubmit={onSubmit}>
+        <div className="stepper stepper--weight">
+          <input type="number" value={weight} placeholder="weight" onChange={handleChange}></input>
+          <i className="fa fa-plus" onClick={stepUp}></i>
+          <i className="fa fa-minus" onClick={stepDown}></i>
+        </div>
+
+        <div className="stepper stepper--reps">
+          <input type="number" id='reps' value={reps} placeholder="reps" onChange={handleChange}></input>
+          <i className="fa fa-plus" onClick={stepUp}></i>
+          <i className="fa fa-minus" onClick={stepDown}></i>
+        </div>
+
+        <input type="submit" value="+"></input>
+      </form>
+  )
 }
 
 export default AddSet
