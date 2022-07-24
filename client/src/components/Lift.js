34a844fb9bebf30da88e4b13853d9b638e94d862
Alex <alex.kliger@gmail.com>
2022-07-24 12:18:44 -0400

    styled lift component

--- client/src/components/Lift.js
+++ client/src/components/Lift.js
@@ -12,11 +12,13 @@ const Lift = ({lift, updateLift, deleteLift}) => {
     return (
         <li className="lift">
             <i className="fa fa-trash" onClick={deleteLift}></i>
-            <h3 className="lift__name">{lift.name}</h3>
+            <h3 className="lift__name font-size--large">{lift.name}</h3>
+            <div className="lift__sets">
             {lift.sets.map(set => (
-                <span>{set} </span>
+                <span>{set}</span>
             ))}
-            <span><i onClick={deleteSet} className="fa fa-minus"></i></span>
+                {lift.sets.length > 0 && <span><i onClick={deleteSet} className="fa fa-minus"></i></span>}
+            </div>
             <AddSet id={lift._id} handleSubmit={addSet} />
         </li>
     )
