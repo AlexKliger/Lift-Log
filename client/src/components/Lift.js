3a73e75a9ecd90a8641d6a24f79b5982c4b9091e
Alex <alex.kliger@gmail.com>
2022-08-14 13:40:50 -0400

    lift__header styling

--- client/src/components/Lift.js
+++ client/src/components/Lift.js
@@ -11,17 +11,19 @@ const Lift = ({lift, updateLift, deleteLift}) => {
     }, [lift])
 
     return (
-        <li className="lift">
-            <i className="fa fa-trash" onClick={() => deleteLift(lift._id)}></i>
-            <h3 className="lift__name font-size--large">{lift.name}</h3>
-            <div className="lift__sets">
-            {lift.sets.map((set, key) => (
-                <span key={key}>{set}</span>
-            ))}
-                {lift.sets.length > 0 && <span><i onClick={deleteSet} className="fa fa-minus"></i></span>}
-            </div>
-            <AddSet handleSubmit={addSet} />
-        </li>
+      <li className="lift">
+        <div className="lift__header">
+          <i className="fa fa-trash" onClick={() => deleteLift(lift._id)}></i>
+          <h3 className="lift__name font-size--large">{lift.name}</h3>
+        </div>
+        <div className="lift__sets">
+        {lift.sets.map((set, key) => (
+          <span key={key}>{set}</span>
+        ))}
+          {lift.sets.length > 0 && <span><i onClick={deleteSet} className="fa fa-minus"></i></span>}
+        </div>
+        <AddSet handleSubmit={addSet} />
+      </li>
     )
 }
 
