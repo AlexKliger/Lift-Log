612d0604a9f641c7ee62f564c0f14f3f241d684d
Alex <alex.kliger@gmail.com>
2022-08-16 19:41:50 -0400

    applied background color and box-shadow classes to Dropdown component

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -1,11 +1,11 @@
 const Dropdown = ({ visible, user, logout }) => {
   return (
-    <div className={`dropdown ${visible ? "": "dropdown--hidden"}`}>
-    <h3 className="font-size--large">User: {user ? user.username : 'none'}</h3>
-    
-    {user && <button onClick={logout}>Logout</button>}
-  </div>
-    )
+    <div className={`dropdown bg-color--primary box-shadow ${visible ? "": "dropdown--hidden"}`}>
+      <h3 className="font-size--large">User: {user ? user.username : 'none'}</h3>
+      
+      {user && <button onClick={logout}>Logout</button>}
+    </div>
+  )
 }
 
 export default Dropdown
