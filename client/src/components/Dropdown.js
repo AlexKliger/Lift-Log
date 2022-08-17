0751ec3d9dec7383ecd885f5381d83aabfaf33f6
Alex <alex.kliger@gmail.com>
2022-08-16 20:41:01 -0400

    nav working build

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -2,8 +2,12 @@ const Dropdown = ({ visible, user, logout }) => {
   return (
     <div className={`dropdown bg-color--primary box-shadow ${visible ? "": "dropdown--hidden"}`}>
       <h3 className="font-size--large">User: {user ? user.username : 'none'}</h3>
-      
-      {user && <button onClick={logout}>Logout</button>}
+      <nav>
+        <ul>
+          <li>Settings</li>
+          <li onClick={logout}><a>Logout</a></li>
+        </ul>
+      </nav>
     </div>
   )
 }
