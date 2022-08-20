e7ef29548daa03a6ae1a89d6a543dfdb14e18b87
Alex <alex.kliger@gmail.com>
2022-08-20 13:11:34 -0400

    moved add-workout to header

--- client/src/components/Log.js
+++ client/src/components/Log.js
@@ -1,15 +1,9 @@
 import Workout from './Workout'
-import AddItem from './AddItem'
 
-const Log = ({workouts, createWorkout, deleteWorkout, updateWorkout, updateLift}) => {
+const Log = ({workouts, deleteWorkout, updateWorkout, updateLift}) => {
 
   return(
     <main className="l-centered">
-      <AddItem
-        handleSubmit={createWorkout}
-        placeholder="Add workout"
-        styling="add-workout"
-      />
       {!workouts
       ?
       'Loading...'
