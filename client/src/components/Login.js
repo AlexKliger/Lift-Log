3bc0192e8a0ad7f80c37c8a0a0f346705ffd4c96
Alex <alex.kliger@gmail.com>
2022-08-26 11:09:45 -0400

    moved component functions to useCallback callbacks

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,50 +1,50 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { getUser, login } from '../util/api'
 
 const Login = ({ setWorkouts, setUser, user }) => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
-    function handleChange(e) {
-      if (e.target.id === 'username') setUsername(e.target.value)
-      if (e.target.id === 'password') setPassword(e.target.value)
-    }
+  const handleChange = useCallback((e) => {
+    if (e.target.id === 'username') setUsername(e.target.value)
+    if (e.target.id === 'password') setPassword(e.target.value)
+  })
 
-    async function onSubmit(e) {
-      e.preventDefault()
-      setWorkouts(await login(username, password))
-      setUser(await getUser())
-    }
+  const onSubmit = useCallback(async (e) => {
+    e.preventDefault()
+    setWorkouts(await login(username, password))
+    setUser(await getUser())
+  })
 
-    return (
-        <form onSubmit={onSubmit}>
-          <div>
-            <label htmlFor="username">Username</label>
-            <input
-              onChange={handleChange}
-              id="username"
-              name="username"
-              type="text"
-              value={username}
-              autoComplete="username"
-              required />
-          </div>
-          <div>
-            <label htmlFor="current-password">Password</label>
-            <input
-              onChange={handleChange}
-              id="password"
-              name="password"
-              type="password"
-              value={password}
-              autoComplete="current-password"
-              required />
-          </div>
-          <div>
-            <button type="submit">Sign in</button>
-          </div>
-        </form>
-    )
+  return (
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            onChange={handleChange}
+            id="username"
+            name="username"
+            type="text"
+            value={username}
+            autoComplete="username"
+            required />
+        </div>
+        <div>
+          <label htmlFor="current-password">Password</label>
+          <input
+            onChange={handleChange}
+            id="password"
+            name="password"
+            type="password"
+            value={password}
+            autoComplete="current-password"
+            required />
+        </div>
+        <div>
+          <button type="submit">Sign in</button>
+        </div>
+      </form>
+  )
 }
 
 export default Login
