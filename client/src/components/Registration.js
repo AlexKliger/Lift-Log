acc7bb26dc94ac79c00e32ba34429e46088ceec4
Alex <alex.kliger@gmail.com>
2022-08-29 12:42:37 -0400

    added option to register as temporary guest

--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -1,4 +1,5 @@
 import { useCallback, useState } from "react"
+import { uid } from 'uid'
 import { getUser, login, register } from "../util/api"
 
 const Registration = ({ setWorkouts, setUser }) => {
@@ -11,18 +12,28 @@ const Registration = ({ setWorkouts, setUser }) => {
     setCredentials(newCreds)
   })
 
-  const handleSubmit = useCallback(async (e) => {
-    e.preventDefault()
-    const registrationStatus = await register(credentials.username, credentials.password)
+  const attemptRegistering = useCallback(async (username, password) => {
+    const registrationStatus = await register(username, password)
     if (registrationStatus.success) {
-      setWorkouts(await login(credentials.username, credentials.password))
+      setWorkouts(await login(username, password))
       setUser(await getUser())
     }
     setDisplayMessage(registrationStatus.message)
-  })
+  }, [])
+
+  const handleSubmit = useCallback(async (e) => {
+    e.preventDefault()
+    await attemptRegistering(credentials.username, credentials.password)
+  }, [credentials])
+
+  const handleGuestClick = useCallback(async () => {
+    const guestId = uid()
+    await attemptRegistering(`guest${guestId}`, 'guest')
+  }, [])
 
   return (
     <form onSubmit={handleSubmit}>
+      <span onClick={handleGuestClick}>Continue as Guest</span>
       <h2 className="font-size--large">Register</h2>
       <span>{displayMessage}</span>
 
