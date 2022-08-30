129c02ee64d1543cdc7f5c53a0695a07e538c0a6
Alex <alex.kliger@gmail.com>
2022-08-30 14:15:10 -0400

    dropdown menu now only visible when a user is logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,13 +11,16 @@ const Header = ({setWorkouts, setUser, user, setDropdown, dropdown }) => {
     <>
     <header className="header color-bg--secondary">
       <h1 className="header__title font-size--header">{user ? user.username : "Lifter"}'s Log</h1>
-
+      {user
+      &&
       <div>
         <Link to="/edit/new">
-          {user && <i className="fa fa-plus color-font--primary font-size--large"></i>}
+          <i className="fa fa-plus color-font--primary font-size--large"></i>
         </Link>
         <i onClick={() => setDropdown({...dropdown, visible: !dropdown.visible, content: <MainNav user={user} logout={logout} theme={theme} setTheme={setTheme} setWorkouts={setWorkouts} setUser={setUser} />})} className="dropdown-toggle fa fa-ellipsis-v font-size--large"></i>
       </div>
+      }
+
     </header>
 
     <div style={{"height": "100px"}}>
